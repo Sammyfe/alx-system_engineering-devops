bb934a6052d7dee3a628fccb4afa697cd5d1b297
sammyyfe <oluwayanmife2@gmail.com>
2022-09-18 16:48:31 -0700

    corrected

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # using puppet to create a file in /tmp
 
 file { '0-create_a_file':
@@ -7,13 +6,4 @@ file { '0-create_a_file':
   group   => 'www-data',
   mode    => '0744',
   content => 'I love Puppet',
-=======
-# creates a file in /tmp
-
-file { '/tmp/school':
-  content =>'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
->>>>>>> f808acfb3748309f02fd3c82e1f871b49ac38cb2
 }
